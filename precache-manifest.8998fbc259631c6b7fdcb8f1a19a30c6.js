de0216e658b1d2d3a7d7a5fe9382b5f3f9cf5612
JB <jonathan-best@users.noreply.github.com>
2019-04-13 09:32:02 +0100

    Updates

---
+++ precache-manifest.8998fbc259631c6b7fdcb8f1a19a30c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a63027a44e34c60b8da1",
+    "url": "/arcade/static/js/runtime~main.199437c3.js"
+  },
+  {
+    "revision": "35129e339702751ea744",
+    "url": "/arcade/static/js/main.6c0540fc.chunk.js"
+  },
+  {
+    "revision": "9bea828446b207cfe1ef",
+    "url": "/arcade/static/js/2.ed1ab5c5.chunk.js"
+  },
+  {
+    "revision": "35129e339702751ea744",
+    "url": "/arcade/static/css/main.c2505cf6.chunk.css"
+  },
+  {
+    "revision": "62c8384b08d47bbf063289dfa9e5e1d4",
+    "url": "/arcade/index.html"
+  }
+];
