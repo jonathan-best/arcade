bd0bcea950c108eaf53b9b5b0ec886b14cb96032
JB <jonathan-best@users.noreply.github.com>
2019-06-01 12:51:46 +0100

    Updates

---
+++ precache-manifest.8b7a7d453f69a4c525a93cbf60c76d33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff85f9601aa1b48b165a",
+    "url": "/arcade/static/js/runtime~main.dcb9f9b6.js"
+  },
+  {
+    "revision": "5bbfc937a7b668624224",
+    "url": "/arcade/static/js/main.9cb6a3e1.chunk.js"
+  },
+  {
+    "revision": "77c7bb2ce89c43d4812f",
+    "url": "/arcade/static/js/5.275fdb59.chunk.js"
+  },
+  {
+    "revision": "39da668e1b4b7f457fce",
+    "url": "/arcade/static/js/4.4f3ceadc.chunk.js"
+  },
+  {
+    "revision": "dde0da1739cb92844266",
+    "url": "/arcade/static/js/3.f9c33620.chunk.js"
+  },
+  {
+    "revision": "1cc1bea6d77d9b8d9804",
+    "url": "/arcade/static/js/0.493d8c66.chunk.js"
+  },
+  {
+    "revision": "5bbfc937a7b668624224",
+    "url": "/arcade/static/css/main.bf76adbd.chunk.css"
+  },
+  {
+    "revision": "7ca251bb54bf5150ef4c940849a30376",
+    "url": "/arcade/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/arcade/precache-manifest.20caf943eb19c0224c44004554988fc0.js"
+  "/arcade/precache-manifest.8b7a7d453f69a4c525a93cbf60c76d33.js"
 );
 
 workbox.clientsClaim();
